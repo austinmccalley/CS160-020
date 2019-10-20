9ce06d87d0b92eedc7fbd24f9158916d3f3a3bc9
Austin McCalley <austin@austinmccalley.com>
2019-10-19 18:58:36 -0700

    Added final assignment4.py

--- assignments/assignment004/assignment4.py
+++ assignments/assignment004/assignment4.py
@@ -43,6 +43,40 @@ def decmToBinary():
         print('WARNING: Did not get a number input! \nGot exception %s' % e)
         #sys.exit(1)
 
+def arrIsBinary(arr):
+    for x in range(0, len(arr)):
+        if arr[x] != '0' and arr[x] != '1':
+            return False
+    return True
+
+def binaryToDecimal():
+    try:
+        binary = int(input('Please enter the binary number which you would like to conver to decimal: '))
+
+        bin_arr = list(str(binary))
+        if not arrIsBinary(bin_arr):
+            print('The input contains numbers other than 1 and 0, please check the input')
+            return binaryToDecimal()
+        bin_arr.reverse()
+        bin_len = len(bin_arr)
+
+        curr_num = binary
+
+        decm_num = []
+        for x in range(0, bin_len):
+            #print(str(int(bin_arr[x]) * 2**x))
+            decm_num.append(int(bin_arr[x]) * 2**x)
+
+        decm_total = sum(decm_num)
+
+        return decm_total
+    except Exception as e:
+        print('We got an exception %s!' % e)
+
+
+
+
+
 def doMath():
     try:
         num1 = float(input('Enter your first operand: '))
@@ -83,10 +117,21 @@ while not exit_con:
         In programmer mode, the user can enter any unsigned decimal number to convert to binary.
         Hint: you could find the left starting bit by increasing the exponent while the number is greater than equal to 2** exponent.
         '''
-        bin_num = decmToBinary()
-        if bin_num is not None:
-            print('The binary equivalent is %s' % str(bin_num))
-        #print('The binary equivalent is %s!' % str(decmToBinary()))
+        try:
+            programmer_mode = int(input("Do you want to convert decimal to binary(0) or binary to decimal(1)? "))
+
+            if programmer_mode == 0:
+                bin_num = decmToBinary()
+                if bin_num is not None:
+                    print('The binary equivalent is %s' % str(bin_num))
+            elif programmer_mode == 1:
+                decm_num = binaryToDecimal()
+                if decm_num is not None:
+                    print('The decimal equivalent is %s' % str(decm_num))
+            else:
+                print('Next time please specify either 0 or 1 depending on which mode you would like!')
+        except Exception as e:
+            print('We got an exception, %s' % e)
     elif curr_mode == 1:
         #Scientific
         '''
